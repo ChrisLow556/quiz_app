d93b7f9c6f3d26d7e70746486dd21c41ef0b0258
ChrisLow556 <chrislowxiaohiang@gmail.com>
2025-03-30 17:47:56 +0800

    Fix problem where UI goes out of bounds whenever device is rotated using Orientation builder

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -28,36 +28,58 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   Widget build(BuildContext context) {
     final currentQuestion = questions[currentQuestionIndex];
 
-    return SizedBox(
-      width: double.infinity,
-      child: Container(
-        margin: const EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              currentQuestion.text,
-              style: GoogleFonts.lato(
-                color: const Color.fromARGB(255, 200, 222, 255),
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        return SizedBox(
+          width: double.infinity,
+          child: Container(
+            margin: const EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  currentQuestion.text,
+                  style: GoogleFonts.lato(
+                    color: const Color.fromARGB(255, 200, 222, 255),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                if (orientation == Orientation.portrait)
+                  ...currentQuestion.getShuffledAnswers().map(
+                        (answer) => AnswerButton(
+                          answerText: answer,
+                          onTap: () {
+                            answerQuestion(answer);
+                          },
+                        ),
+                      )
+                else
+                  Wrap(
+                    spacing: 12,
+                    runSpacing: 12,
+                    children: currentQuestion
+                        .getShuffledAnswers()
+                        .map(
+                          (answer) => SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.4,
+                            child: AnswerButton(
+                              answerText: answer,
+                              onTap: () {
+                                answerQuestion(answer);
+                              },
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+              ],
             ),
-            const SizedBox(height: 30),
-            ...currentQuestion.getShuffledAnswers().map(
-              (answer) {
-                return AnswerButton(
-                  answerText: answer,
-                  onTap: () {
-                    answerQuestion(answer);
-                  },
-                );
-              },
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
